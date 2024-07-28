45f7b1dc4403e926e7d112b6d3d818be8d3bbac9
ChaqChaq Hamza <hamza.chaqchaq@usmba.ac.ma>
2024-07-28 14:36:47 +0100

    Fixed an error about accessKey

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,18 +1,18 @@
-const  core = require('@actions/core')
+const core = require("@actions/core");
 // const github = require('@actions/github')
-const exec = require('@actions/exec')
+const exec = require("@actions/exec");
 
-function run(){
+function run() {
   // 1) Get inputs
-  const bucket = core.getInput('bucket',{ required: true });
-  const bucketRegion = core.getInput('bucket-region', { required : true });
-  const distFolder = core.getInput('dist-folder', { required:true })
-  
-  // 2) Upload files
-  const s3Uri = `s3://${bucket}` 
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
 
-  core.notice('Hello from JS!')
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  core.notice("Hello from JS!");
 }
 
 run();
